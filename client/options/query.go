76a3a4280794a32bfd30cc89064c262d14098c71
Srinivas Baride <srini@exidio.co>
2023-12-11 01:20:36 +0530

    feat: add pagination related query options

--- client/options/query.go
+++ client/options/query.go
@@ -15,12 +15,17 @@ const (
 
 // QueryOptions represents options for making queries.
 type QueryOptions struct {
-	Height     int64         `json:"height,omitempty"`      // Block height for the query
-	MaxRetries int           `json:"max_retries,omitempty"` // Maximum number of retries for the query
-	Prove      bool          `json:"prove,omitempty"`       // Whether to include proofs in the query response
-	RPCAddr    string        `json:"rpc_addr,omitempty"`    // RPC address for the query
-	Timeout    time.Duration `json:"timeout,omitempty"`     // Timeout for the query
-	WSEndpoint string        `json:"ws_endpoint,omitempty"` // WebSocket endpoint for the query
+	Height         int64         `json:"height,omitempty"`
+	MaxRetries     int           `json:"max_retries,omitempty"`
+	PageCountTotal bool          `json:"page_count_total,omitempty"`
+	PageKey        []byte        `json:"page_key,omitempty"`
+	PageLimit      uint64        `json:"page_limit,omitempty"`
+	PageOffset     uint64        `json:"page_offset,omitempty"`
+	PageReverse    bool          `json:"page_reverse,omitempty"`
+	Prove          bool          `json:"prove,omitempty"`
+	RPCAddr        string        `json:"rpc_addr,omitempty"`
+	Timeout        time.Duration `json:"timeout,omitempty"`
+	WSEndpoint     string        `json:"ws_endpoint,omitempty"`
 }
 
 // Query initializes and returns a new QueryOptions with default values.
@@ -32,9 +37,59 @@ func Query() *QueryOptions {
 	}
 }
 
+// ABCIQueryOptions returns an ABCIQueryOptions instance based on the current QueryOptions.
+func (q *QueryOptions) ABCIQueryOptions() client.ABCIQueryOptions {
+	return client.ABCIQueryOptions{
+		Height: q.Height,
+		Prove:  q.Prove,
+	}
+}
+
+// WithHeight sets the block height for the query.
+func (q *QueryOptions) WithHeight(v int64) *QueryOptions {
+	q.Height = v
+	return q
+}
+
 // WithMaxRetries sets the maximum number of retries for the query.
 func (q *QueryOptions) WithMaxRetries(v int) *QueryOptions {
 	q.MaxRetries = v
+	return q
+}
+
+// WithPageCountTotal sets whether to include the total number of pages in the query response.
+func (q *QueryOptions) WithPageCountTotal(v bool) *QueryOptions {
+	q.PageCountTotal = v
+	return q
+}
+
+// WithPageKey sets the page key for the query.
+func (q *QueryOptions) WithPageKey(v []byte) *QueryOptions {
+	q.PageKey = v
+	return q
+}
+
+// WithPageLimit sets the page limit for the query.
+func (q *QueryOptions) WithPageLimit(v uint64) *QueryOptions {
+	q.PageLimit = v
+	return q
+}
+
+// WithPageOffset sets the page offset for the query.
+func (q *QueryOptions) WithPageOffset(v uint64) *QueryOptions {
+	q.PageOffset = v
+	return q
+}
+
+// WithPageReverse sets whether to reverse the order of the pages in the query response.
+func (q *QueryOptions) WithPageReverse(v bool) *QueryOptions {
+	q.PageReverse = v
+	return q
+}
+
+// WithProve sets whether to include proofs in the query response.
+func (q *QueryOptions) WithProve(v bool) *QueryOptions {
+	q.Prove = v
 	return q
 }
 
@@ -50,28 +105,8 @@ func (q *QueryOptions) WithTimeout(v time.Duration) *QueryOptions {
 	return q
 }
 
-// WithProve sets whether to include proofs in the query response.
-func (q *QueryOptions) WithProve(v bool) *QueryOptions {
-	q.Prove = v
-	return q
-}
-
-// WithHeight sets the block height for the query.
-func (q *QueryOptions) WithHeight(v int64) *QueryOptions {
-	q.Height = v
-	return q
-}
-
 // WithWSEndpoint sets the WebSocket endpoint for the query.
 func (q *QueryOptions) WithWSEndpoint(v string) *QueryOptions {
 	q.WSEndpoint = v
 	return q
-}
-
-// ABCIQueryOptions returns an ABCIQueryOptions instance based on the current QueryOptions.
-func (q *QueryOptions) ABCIQueryOptions() client.ABCIQueryOptions {
-	return client.ABCIQueryOptions{
-		Height: q.Height,
-		Prove:  q.Prove,
-	}
 }
