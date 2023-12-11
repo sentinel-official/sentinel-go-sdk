3a584d86e2c3ee8b3525f46c93ac1a9466bee042
Srinivas Baride <srini@exidio.co>
2023-12-11 19:54:37 +0530

    feat: add accounts method for querying the accounts

--- client/auth.go
+++ client/auth.go
@@ -9,32 +9,61 @@ import (
 	"github.com/sentinel-official/sentinel-sdk/v1/client/options"
 )
 
-// Account queries the account information for a given account address using gRPC.
+// Account queries and returns an account using the given address and options.
+// It uses gRPC to send a request to the "/cosmos.auth.v1beta1.Query/Account" endpoint.
+// The result is an authtypes.AccountI interface and an error if the query fails.
 func (c *Context) Account(ctx context.Context, accAddr sdk.AccAddress, opts *options.QueryOptions) (res authtypes.AccountI, err error) {
-	// gRPC method for querying account information
-	method := "/cosmos.auth.v1beta1.Query/Account"
+	// Initialize variables for the query.
+	var (
+		resp   authtypes.QueryAccountResponse
+		method = "/cosmos.auth.v1beta1.Query/Account"
+		req    = &authtypes.QueryAccountRequest{
+			Address: accAddr.String(),
+		}
+	)
 
-	// Create a request for querying the account
-	req := &authtypes.QueryAccountRequest{
-		Address: accAddr.String(),
-	}
-
-	// Query the account information using the generic QueryGRPC method
-	resp, err := c.QueryGRPC(ctx, method, req, opts)
-	if err != nil {
+	// Send a gRPC query using the provided context, method, request, response, and options.
+	if err := c.QueryGRPC(ctx, method, req, &resp, opts); err != nil {
 		return nil, err
 	}
 
-	// If the response is nil, return nil
-	if resp == nil {
-		return nil, nil
-	}
-
-	// Unmarshal the response value into an authtypes.AccountI interface
-	if err := c.UnmarshalInterface(resp.Value, &res); err != nil {
+	// Unpack the Any type account from the response.
+	if err := c.UnpackAny(resp.Account, &res); err != nil {
 		return nil, err
 	}
 
-	// Return the unmarshalled account information
+	// Return the account and a nil error.
+	return res, nil
+}
+
+// Accounts queries and returns a list of accounts using the given options.
+// It uses gRPC to send a request to the "/cosmos.auth.v1beta1.Query/Accounts" endpoint.
+// The result is a slice of authtypes.AccountI and an error if the query fails.
+func (c *Context) Accounts(ctx context.Context, opts *options.QueryOptions) (res []authtypes.AccountI, err error) {
+	// Initialize variables for the query.
+	var (
+		resp   authtypes.QueryAccountsResponse
+		method = "/cosmos.auth.v1beta1.Query/Accounts"
+		req    = &authtypes.QueryAccountsRequest{
+			Pagination: opts.PageRequest(),
+		}
+	)
+
+	// Send a gRPC query using the provided context, method, request, response, and options.
+	if err := c.QueryGRPC(ctx, method, req, &resp, opts); err != nil {
+		return nil, err
+	}
+
+	// Initialize a slice to store the accounts.
+	res = make([]authtypes.AccountI, 0, len(resp.Accounts))
+
+	// Unpack each Any type account from the response and add it to the result slice.
+	for i := 0; i < len(resp.Accounts); i++ {
+		if err := c.UnpackAny(resp.Accounts[i], &res[i]); err != nil {
+			return nil, err
+		}
+	}
+
+	// Return the list of accounts and a nil error.
 	return res, nil
 }
