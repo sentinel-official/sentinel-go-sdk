7854c6b87a201d98cb2c14789e695a29e0e5c1b2
Tony Stark <ironman0x7b2@protonmail.com>
2025-01-27 00:32:09 +0530

    chore: add SetForFlags method for config in v2ray and wireguard modules

--- v2ray/config.go
+++ v2ray/config.go
@@ -5,22 +5,29 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/spf13/pflag"
+
 	"github.com/sentinel-official/sentinel-go-sdk/types"
 	"github.com/sentinel-official/sentinel-go-sdk/utils"
 )
 
+// Embed the template files for V2Ray configurations.
+//
 //go:embed *.tmpl
 var fs embed.FS
 
 // ClientConfig represents the V2Ray client configuration options.
 type ClientConfig struct{}
 
+// WriteToFile writes the client configuration to a file.
 func (c *ClientConfig) WriteToFile(name string) error {
+	// Read the client configuration template file.
 	text, err := fs.ReadFile("client.json.tmpl")
 	if err != nil {
 		return fmt.Errorf("failed to read template: %w", err)
 	}
 
+	// Execute the template and write it to the specified file.
 	if err := utils.ExecTemplateToFile(string(text), c, name); err != nil {
 		return fmt.Errorf("failed to execute template to file: %w", err)
 	}
@@ -30,14 +37,15 @@ func (c *ClientConfig) WriteToFile(name string) error {
 
 // InboundServerConfig represents the V2Ray inbound server configuration options.
 type InboundServerConfig struct {
-	Port        string `mapstructure:"port"`
-	Proxy       string `mapstructure:"proxy"`
-	Security    string `mapstructure:"security"`
-	TLSCertPath string `mapstructure:"tls_cert_path"`
-	TLSKeyPath  string `mapstructure:"tls_key_path"`
-	Transport   string `mapstructure:"transport"`
+	Port        string `mapstructure:"port"`          // Port defines the inbound port range.
+	Proxy       string `mapstructure:"proxy"`         // Proxy defines the protocol used (e.g., vmess).
+	Security    string `mapstructure:"security"`      // Security specifies the encryption method.
+	TLSCertPath string `mapstructure:"tls_cert_path"` // TLSCertPath specifies the path to the TLS certificate.
+	TLSKeyPath  string `mapstructure:"tls_key_path"`  // TLSKeyPath specifies the path to the TLS private key.
+	Transport   string `mapstructure:"transport"`     // Transport specifies the transport protocol.
 }
 
+// InPort returns the inbound port range.
 func (c *InboundServerConfig) InPort() string {
 	v, err := types.NewPortFromString(c.Port)
 	if err != nil {
@@ -47,6 +55,7 @@ func (c *InboundServerConfig) InPort() string {
 	return fmt.Sprintf("%d-%d", v.InFrom, v.InTo)
 }
 
+// OutPort returns the outbound port range.
 func (c *InboundServerConfig) OutPort() string {
 	v, err := types.NewPortFromString(c.Port)
 	if err != nil {
@@ -71,20 +80,28 @@ func (c *InboundServerConfig) Tag() *Tag {
 
 // Validate validates the InboundServerConfig fields.
 func (c *InboundServerConfig) Validate() error {
+	// Ensure Port is not empty.
 	if c.Port == "" {
 		return errors.New("port cannot be empty")
 	}
+
+	// Validate the Port value.
 	if _, err := types.NewPortFromString(c.Port); err != nil {
 		return fmt.Errorf("invalid port: %w", err)
 	}
+
+	// Validate the Proxy protocol.
 	if v := NewProxyProtocolFromString(c.Proxy); !v.IsValid() {
 		return fmt.Errorf("invalid proxy %s", v)
 	}
 
+	// Validate the Security setting.
 	security := NewTransportSecurityFromString(c.Security)
 	if !security.IsValid() {
 		return fmt.Errorf("invalid security %s", security)
 	}
+
+	// Ensure TLS paths are provided if Security is TLS.
 	if security == TransportSecurityTLS {
 		if c.TLSCertPath == "" {
 			return errors.New("tls_cert_path cannot be empty")
@@ -94,6 +111,7 @@ func (c *InboundServerConfig) Validate() error {
 		}
 	}
 
+	// Validate the Transport protocol.
 	if v := NewTransportProtocolFromString(c.Transport); !v.IsValid() {
 		return fmt.Errorf("invalid transport %s", v)
 	}
@@ -103,35 +121,42 @@ func (c *InboundServerConfig) Validate() error {
 
 // ServerConfig represents the V2Ray server configuration options.
 type ServerConfig struct {
-	Inbounds []InboundServerConfig `mapstructure:"inbounds"`
+	Inbounds []*InboundServerConfig `mapstructure:"inbounds"` // Inbounds is a list of inbound server configurations.
 }
 
 // Validate validates the ServerConfig fields.
 func (c *ServerConfig) Validate() error {
+	// Ensure Inbounds is not empty.
 	if len(c.Inbounds) == 0 {
 		return errors.New("inbounds cannot be empty")
 	}
 
+	// Create sets to track unique inbound and outbound ports and tags.
 	inPortSet := make(map[uint16]bool)
 	outPortSet := make(map[uint16]bool)
 	tagSet := make(map[string]bool)
 
+	// Validate each InboundServerConfig.
 	for _, inbound := range c.Inbounds {
 		if err := inbound.Validate(); err != nil {
 			return fmt.Errorf("invalid inbound: %w", err)
 		}
 
+		// Parse the port range and check for duplicates.
 		port, err := types.NewPortFromString(inbound.Port)
 		if err != nil {
 			panic(err)
 		}
 
+		// Check inbound ports for duplicates.
 		for p := port.InFrom; p <= port.InTo; p++ {
 			if inPortSet[p] {
 				return fmt.Errorf("duplicate in port %d", p)
 			}
 			inPortSet[p] = true
 		}
+
+		// Check outbound ports for duplicates.
 		for p := port.OutFrom; p <= port.OutTo; p++ {
 			if outPortSet[p] {
 				return fmt.Errorf("duplicate out port %d", p)
@@ -139,6 +164,7 @@ func (c *ServerConfig) Validate() error {
 			outPortSet[p] = true
 		}
 
+		// Check tags for duplicates.
 		tag := inbound.Tag().String()
 		if tagSet[tag] {
 			return fmt.Errorf("duplicate tag %s", tag)
@@ -149,12 +175,15 @@ func (c *ServerConfig) Validate() error {
 	return nil
 }
 
+// WriteToFile writes the server configuration to a file.
 func (c *ServerConfig) WriteToFile(name string) error {
+	// Read the server configuration template file.
 	text, err := fs.ReadFile("server.json.tmpl")
 	if err != nil {
 		return fmt.Errorf("failed to read template: %w", err)
 	}
 
+	// Execute the template and write it to the specified file.
 	if err := utils.ExecTemplateToFile(string(text), c, name); err != nil {
 		return fmt.Errorf("failed to execute template to file: %w", err)
 	}
@@ -162,9 +191,13 @@ func (c *ServerConfig) WriteToFile(name string) error {
 	return nil
 }
 
-func DefaultServerConfig() ServerConfig {
-	return ServerConfig{
-		Inbounds: []InboundServerConfig{
+// SetForFlags adds server configuration flags to the specified FlagSet.
+func (c *ServerConfig) SetForFlags(_ *pflag.FlagSet) {}
+
+// DefaultServerConfig creates a default ServerConfig with predefined values.
+func DefaultServerConfig() *ServerConfig {
+	return &ServerConfig{
+		Inbounds: []*InboundServerConfig{
 			{
 				Port:        fmt.Sprintf("%d", utils.RandomPort()),
 				Proxy:       "vmess",
--- wireguard/config.go
+++ wireguard/config.go
@@ -8,10 +8,14 @@ import (
 	"os"
 	"strings"
 
+	"github.com/spf13/pflag"
+
 	"github.com/sentinel-official/sentinel-go-sdk/types"
 	"github.com/sentinel-official/sentinel-go-sdk/utils"
 )
 
+// Embed the template files for WireGuard configurations.
+//
 //go:embed *.tmpl
 var fs embed.FS
 
@@ -23,17 +27,20 @@ func (c *ClientConfig) Validate() error {
 	return nil
 }
 
-// WriteToFile writes the template to a file using the ClientConfig structure.
+// WriteToFile writes the client configuration template to a file.
 func (c *ClientConfig) WriteToFile(name string) error {
+	// Read the client configuration template file.
 	text, err := fs.ReadFile("client.conf.tmpl")
 	if err != nil {
 		return fmt.Errorf("failed to read template: %w", err)
 	}
 
+	// Execute the template and write it to the specified file.
 	if err := utils.ExecTemplateToFile(string(text), c, name); err != nil {
 		return fmt.Errorf("failed to execute template to file: %w", err)
 	}
 
+	// Change file permissions to read-only.
 	if err := os.Chmod(name, 0400); err != nil {
 		return fmt.Errorf("failed to change file permissions: %w", err)
 	}
@@ -43,15 +50,15 @@ func (c *ClientConfig) WriteToFile(name string) error {
 
 // ServerConfig represents the WireGuard server configuration.
 type ServerConfig struct {
-	InInterface  string `mapstructure:"in_interface"`
-	IPv4Addr     string `mapstructure:"ipv4_addr"`
-	IPv6Addr     string `mapstructure:"ipv6_addr"`
-	OutInterface string `mapstructure:"out_interface"`
-	Port         string `mapstructure:"port"`
-	PrivateKey   string `mapstructure:"private_key"`
+	InInterface  string `mapstructure:"in_interface"`  // InInterface specifies the inbound interface.
+	IPv4Addr     string `mapstructure:"ipv4_addr"`     // IPv4Addr is the IPv4 address with CIDR notation.
+	IPv6Addr     string `mapstructure:"ipv6_addr"`     // IPv6Addr is the IPv6 address with CIDR notation.
+	OutInterface string `mapstructure:"out_interface"` // OutInterface specifies the outbound interface.
+	Port         string `mapstructure:"port"`          // Port specifies the WireGuard listening port.
+	PrivateKey   string `mapstructure:"private_key"`   // PrivateKey is the WireGuard private key.
 }
 
-// Address returns the combined IPv4 and IPv6 Addrs, separated by a comma if both are present.
+// Address returns the combined IPv4 and IPv6 addresses, separated by a comma.
 func (c *ServerConfig) Address() string {
 	var addrs []string
 	if c.IPv4Addr != "" {
@@ -64,6 +71,7 @@ func (c *ServerConfig) Address() string {
 	return strings.Join(addrs, ", ")
 }
 
+// InPort returns the inbound port as a uint16.
 func (c *ServerConfig) InPort() uint16 {
 	v, err := types.NewPortFromString(c.Port)
 	if err != nil {
@@ -73,6 +81,7 @@ func (c *ServerConfig) InPort() uint16 {
 	return v.InFrom
 }
 
+// OutPort returns the outbound port as a uint16.
 func (c *ServerConfig) OutPort() uint16 {
 	v, err := types.NewPortFromString(c.Port)
 	if err != nil {
@@ -82,6 +91,7 @@ func (c *ServerConfig) OutPort() uint16 {
 	return v.OutFrom
 }
 
+// PublicKey returns the public key derived from the private key.
 func (c *ServerConfig) PublicKey() *Key {
 	pk, err := NewKeyFromString(c.PrivateKey)
 	if err != nil {
@@ -93,31 +103,44 @@ func (c *ServerConfig) PublicKey() *Key {
 
 // Validate checks that the ServerConfig fields have valid values.
 func (c *ServerConfig) Validate() error {
+	// Ensure InInterface is not empty.
 	if c.InInterface == "" {
 		return errors.New("in_interface cannot be empty")
 	}
+
+	// Ensure at least one of IPv4Addr or IPv6Addr is provided.
 	if c.IPv4Addr == "" && c.IPv6Addr == "" {
 		return errors.New("either ipv4_addr or ipv6_addr is required")
 	}
+
+	// Validate IPv4Addr if provided.
 	if c.IPv4Addr != "" {
 		if _, err := types.NewNetPrefixFromString(c.IPv4Addr); err != nil {
 			return fmt.Errorf("invalid ipv4_addr: %w", err)
 		}
 	}
+
+	// Validate IPv6Addr if provided.
 	if c.IPv6Addr != "" {
 		if _, err := types.NewNetPrefixFromString(c.IPv6Addr); err != nil {
 			return fmt.Errorf("invalid ipv6_addr: %w", err)
 		}
 	}
+
+	// Ensure OutInterface is not empty.
 	if c.OutInterface == "" {
 		return errors.New("out_interface cannot be empty")
 	}
+
+	// Ensure Port is not empty and validate it.
 	if c.Port == "" {
 		return errors.New("port cannot be empty")
 	}
 	if _, err := types.NewPortFromString(c.Port); err != nil {
 		return fmt.Errorf("invalid port: %w", err)
 	}
+
+	// Ensure PrivateKey is not empty and validate it.
 	if c.PrivateKey == "" {
 		return errors.New("private_key cannot be empty")
 	}
@@ -128,17 +151,20 @@ func (c *ServerConfig) Validate() error {
 	return nil
 }
 
-// WriteToFile writes the template to a file using the ServerConfig structure.
+// WriteToFile writes the server configuration template to a file.
 func (c *ServerConfig) WriteToFile(name string) error {
+	// Read the server configuration template file.
 	text, err := fs.ReadFile("server.conf.tmpl")
 	if err != nil {
 		return fmt.Errorf("failed to read template: %w", err)
 	}
 
+	// Execute the template and write it to the specified file.
 	if err := utils.ExecTemplateToFile(string(text), c, name); err != nil {
 		return fmt.Errorf("failed to execute template to file: %w", err)
 	}
 
+	// Change file permissions to read-only.
 	if err := os.Chmod(name, 0400); err != nil {
 		return fmt.Errorf("failed to change file permissions: %w", err)
 	}
@@ -146,6 +172,7 @@ func (c *ServerConfig) WriteToFile(name string) error {
 	return nil
 }
 
+// IPv4Pool returns the IPv4 address pool.
 func (c *ServerConfig) IPv4Pool() (*types.IPPool, error) {
 	pool, err := types.NewIPPoolFromString(c.IPv4Addr)
 	if err != nil {
@@ -155,6 +182,7 @@ func (c *ServerConfig) IPv4Pool() (*types.IPPool, error) {
 	return pool, nil
 }
 
+// IPv6Pool returns the IPv6 address pool.
 func (c *ServerConfig) IPv6Pool() (*types.IPPool, error) {
 	pool, err := types.NewIPPoolFromString(c.IPv6Addr)
 	if err != nil {
@@ -164,35 +192,46 @@ func (c *ServerConfig) IPv6Pool() (*types.IPPool, error) {
 	return pool, nil
 }
 
+// IPPools returns all address pools (IPv4 and IPv6).
 func (c *ServerConfig) IPPools() ([]*types.IPPool, error) {
 	var pools []*types.IPPool
+
+	// Add the IPv4 pool if IPv4Addr is provided.
 	if c.IPv4Addr != "" {
 		pool, err := c.IPv4Pool()
 		if err != nil {
 			return nil, fmt.Errorf("failed to get ipv4 pool: %w", err)
 		}
 
+		// Append the IPv4 pool to the list.
 		pools = append(pools, pool)
 	}
+
+	// Add the IPv6 pool if IPv6Addr is provided.
 	if c.IPv6Addr != "" {
 		pool, err := c.IPv6Pool()
 		if err != nil {
 			return nil, fmt.Errorf("failed to get ipv6 pool: %w", err)
 		}
 
+		// Append the IPv6 pool to the list.
 		pools = append(pools, pool)
 	}
 
 	return pools, nil
 }
 
-func DefaultServerConfig() ServerConfig {
+// SetForFlags adds server configuration flags to the specified FlagSet.
+func (c *ServerConfig) SetForFlags(_ *pflag.FlagSet) {}
+
+// DefaultServerConfig creates a default ServerConfig with randomized values.
+func DefaultServerConfig() *ServerConfig {
 	pk, err := NewPrivateKey()
 	if err != nil {
 		panic(err)
 	}
 
-	return ServerConfig{
+	return &ServerConfig{
 		InInterface:  "wg0",
 		IPv4Addr:     fmt.Sprintf("10.%d.%d.1/24", rand.Intn(256), rand.Intn(256)),
 		IPv6Addr:     "",
