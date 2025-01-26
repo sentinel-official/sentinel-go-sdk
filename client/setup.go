4cab7a5f2e5f9ce3a0318d772950416b59496813
Tony Stark <ironman0x7b2@protonmail.com>
2025-01-27 00:33:30 +0530

    feat: implement method for setting up the keyring in the base client

---
+++ client/setup.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+
+	"github.com/sentinel-official/sentinel-go-sdk/config"
+)
+
+// SetupKeyring initializes and configures a keyring for cryptographic key management.
+func (c *BaseClient) SetupKeyring(cfg *config.KeyringConfig) error {
+	// Create a keyring instance using the provided configuration.
+	kr, err := keyring.New(cfg.GetName(), cfg.GetBackend(), cfg.GetHomeDir(), cfg.GetInput(), c.protoCodec)
+	if err != nil {
+		return fmt.Errorf("failed to create keyring: %w", err)
+	}
+
+	// Assign the created keyring to the client.
+	c.WithKeyring(kr)
+	return nil
+}
