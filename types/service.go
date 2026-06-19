b534321cacd004037f29f5f31a2b863609ac393f
Tony Stark <ironman0x7b2@protonmail.com>
2026-06-19 07:35:31 +0000

    feat: add xray service type

--- types/service.go
+++ types/service.go
@@ -15,6 +15,7 @@ const (
 	ServiceTypeWireGuard                             // ServiceTypeWireGuard represents the WireGuard service type.
 	ServiceTypeV2Ray                                 // ServiceTypeV2Ray represents the V2Ray service type.
 	ServiceTypeOpenVPN                               // ServiceTypeOpenVPN represents the OpenVPN service type.
+	ServiceTypeXray                                  // ServiceTypeXray represents the Xray service type.
 )
 
 // String returns the string representation of the ServiceType.
@@ -26,6 +27,8 @@ func (s ServiceType) String() string {
 		return "v2ray"
 	case ServiceTypeOpenVPN:
 		return "openvpn"
+	case ServiceTypeXray:
+		return "xray"
 	case ServiceTypeUnspecified:
 		return StringUnspecified
 	default:
@@ -43,6 +46,8 @@ func ServiceTypeFromString(s string) ServiceType {
 		return ServiceTypeV2Ray
 	case "openvpn":
 		return ServiceTypeOpenVPN
+	case "xray":
+		return ServiceTypeXray
 	default:
 		return ServiceTypeUnspecified
 	}
